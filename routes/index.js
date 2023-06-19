d515e763dc281db5599f08dffd4ef19f0bf027cc
Christophe Delobel <cdelobel.ext@simplon.co>
2023-06-19 14:46:31 +0200

    add swagger

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,44 @@ const express = require("express");
 const router = express.Router();
 const blaguesController = require("./../controllers/blagues");
 
+/**
+ * @swagger
+ * /blagues:
+ *   get:
+ *     summary: Retrieve all blagues
+ *     description: Retrieve a list of all blagues
+ *     responses:
+ *       '200':
+ *         description: A list of blagues
+ *       '500':
+ *         description: Internal server error
+ */
 router.get("/blagues", blaguesController.findAll);
-
+/**
+ * @swagger
+ * /blagues/{id}:
+ *   get:
+ *     summary: Retrieve a blague by ID or get a random blague
+ *     description: Retrieve a blague by the specified ID or get a random blague if ID is not provided
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: ID of the blague (optional)
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: random
+ *         description: Flag to indicate whether to retrieve a random blague
+ *         schema:
+ *           type: boolean
+ *     responses:
+ *       '200':
+ *         description: A blague object
+ *       '404':
+ *         description: Blague not found
+ *       '500':
+ *         description: Internal server error
+ */
 router.get("/blagues/:id", blaguesController.findByIdOrRandom);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,30 @@ const port = 3000;
 const cors = require("cors");
 app.use(cors());
 
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+// Swagger JSDoc configuration
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Blagues API",
+      version: "1.0.0",
+      description: "API for retrieving blagues",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api/v1", // Update with your server's URL
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Path to the main server file
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 const router = require("./routes");
 
 app.use("/api/v1", router);
